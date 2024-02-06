3c6558bfaf98c02236ea5d634f770516c42ca991
sayemanadira <slubis@g.hmc.edu>
2024-02-05 19:32:36 -0800

    tryout

--- uploads/app.py
+++ uploads/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, url_for
 
 from flask_uploads import UploadSet, IMAGES, configure_uploads
 from flask_wtf import FlaskForm
@@ -32,7 +32,10 @@ def upload_image():
     form = UploadForm()
     if form.validate_on_submit():
         filename = photos.save(form.photo.data)
-    return render_template('photo_it.html')
+        file_url = url_for('get_file', filename=filename)
+    else:
+        file_url = None
+    return render_template('photo_it.html', form=form, file_url=file_url)
 
 
 if __name__ == "__main__":
