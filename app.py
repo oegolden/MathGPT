cea250d6e76df61b931134e5e1164ae3992b5d81
sayemanadira <slubis@g.hmc.edu>
2024-02-19 18:50:16 -0800

    image dropbox progress

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 
-from flask import Flask, render_template, send_from_directory, url_for, redirect
+from flask import Flask, render_template, send_from_directory, url_for, redirect, flash, request
 
 from flask_uploads import UploadSet, IMAGES, configure_uploads
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
+import os
+import urllib.request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'golden-lubis'
-# app.config['UPLOAD_PHOTOS_DEST'] = 'uploads'
 
-# photos = UploadSet('photos', IMAGES)
-# configure_uploads(app, photos)
+UPLOAD_FOLDER = 'static/uploads/'
 
-# class UploadForm(FlaskForm):
-#     photo = FileField(
-#         validators=[
-#             FileAllowed(photos, 'Only images are allowed'),
-#             FileRequired('File filed should not be empty')
-#         ]
-#     )
-#     submit = SubmitField('Upload')
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSION = set(['png', 'jpg', 'jpeg','gif'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSION
+
 
 @app.route('/')
 def home():
@@ -31,8 +32,32 @@ def math():
     return redirect(url_for("home"))
 
 @app.route('/photoit.html')
-def photo_it():
+def photo():
     return render_template('photoit.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed below')
+        return render_template('photoit.html', filename=filename)
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+ 
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 @app.route('/typeit.html')
 def type():
@@ -41,23 +66,6 @@ def type():
 @app.route('/about.html')
 def about():
     return render_template('about.html')
-
-
-# @app.route('/uploads/<filename>')
-# # def get_file(filename):
-# #     return send_from_directory(app.config['UPLOADED_PHOTOS_DEST'],filename)
-
-
-# @app.route('/', methods=['GET', 'POST'])
-# def upload_image():
-    # form = UploadForm()
-    # if form.validate_on_submit():
-    #     filename = photos.save(form.photo.data)
-    #     file_url = url_for('get_file', filename=filename)
-    # else:
-    #     file_url = None
-    # return render_template('photo_it.html', form=form, file_url=file_url)
-
 
 if __name__ == "__main__":
     app.run()
